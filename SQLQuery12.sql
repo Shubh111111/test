15d2b59637070acd03af92b02683807cd4132cad
shubh111111 <singhlion1211@gmail.com>
2022-10-14 22:49:32 +0530

    sql

---
+++ SQLQuery12.sql
@@ -0,0 +1,18 @@
+create table student1_info
+(
+stu_id int,
+name varchar(50),
+ADHAAR INT,
+Fathers_Name varchar(100),
+mother_name varchar(50),
+email varchar(100),
+phone bigint,
+date_of_admission date,
+ADDRES varchar(100),
+district varchar(50),
+)
+
+
+select * from stu1_info
+
+insert into stu_info values('01', 'arun', '513323275522', 'amit', 'meenu', 'arun@gmail.com', '9090908989', '2022-07-01', 'ward no. 01', 'shahdol') 
